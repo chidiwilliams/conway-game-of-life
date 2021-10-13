39f7aa6b0402d9e02237dfd454336fe0f61f80d8
Chidi Williams <williamschidi1@gmail.com>
2021-10-13 20:54:48 +0100

    Update demo

--- demo.js
+++ demo.js
@@ -1,98 +1,295 @@
-const d3 = require('d3');
 const { next } = require('.');
 
-const width = window.innerWidth;
-const height = window.innerHeight;
+const graphics = (() => {
+  let previousGrid;
 
-const cellWidthInPixels = 20;
+  function render(canvas, grid, cellWidth) {
+    const ctx = canvas.getContext('2d');
 
-let grid = makePlainGrid(
-  Math.ceil(width / cellWidthInPixels),
-  Math.ceil(height / cellWidthInPixels),
-  false,
-);
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        const cell = grid[i][j];
 
-const svg = d3
-  .select('.game__display')
-  .append('svg')
-  .attr('width', '100%')
-  .attr('height', '100%');
-
-const cells = svg.selectAll('g').data(grid);
-addRows(cells);
-
-function addRows(rows) {
-  rows
-    .enter()
-    .append('g')
-    .attr('transform', (d, i) => `translate(0 ${i * cellWidthInPixels})`)
-    .attr('data-row-index', (d, i) => i)
-    .selectAll('.cell')
-    .data((d) => d)
-    .enter()
-    .append('rect')
-    .attr('class', 'cell')
-    .attr('transform', (d, i) => `translate(${i * cellWidthInPixels} 0)`)
-    .classed('live', (d) => d)
-    .attr('data-column-index', (d, i) => i)
-    .attr('width', cellWidthInPixels)
-    .attr('height', cellWidthInPixels)
-    .on('click', (evt, d) => {
-      const colIndex = +evt.target.getAttribute('data-column-index');
-      const rowIndex = +evt.target.parentElement.getAttribute('data-row-index');
-
-      grid = grid.map((row) => [...row]);
-      grid[rowIndex][colIndex] = !d;
-      updateGrid();
-    });
-}
-
-function updateGrid() {
-  const rows = svg.selectAll('g').data(grid, (d, i) => i + d.join(''));
-  rows.exit().remove();
-  addRows(rows);
-}
-
-function makePlainGrid(w, h, v) {
-  const grid = [];
-  for (let i = 0; i < h; i++) {
-    const r = [];
-    for (let j = 0; j < w; j++) {
-      r.push(v);
+        // Only re-draw if cell has changed
+        if (!previousGrid || cell !== previousGrid[i][j]) {
+          ctx.beginPath();
+          ctx.strokeStyle = '#555555';
+          ctx.fillStyle = cell ? '#ffffff' : '#000000';
+          ctx.lineWidth = 1;
+          ctx.rect(j * cellWidth, i * cellWidth, cellWidth, cellWidth);
+          ctx.fill();
+          ctx.stroke();
+        }
+      }
     }
-    grid.push(r);
+
+    previousGrid = grid;
   }
-  return grid;
-}
 
-const generationsCountElement = document.getElementById('generations-count');
-const startButtonElement = document.getElementById('start');
+  function resizeCanvasToDisplaySize(canvas) {
+    const width = canvas.clientWidth;
+    const height = canvas.clientHeight;
 
-let isPlaying = false;
-let gen = 0;
-rafInterval(() => {
-  if (isPlaying) {
-    generationsCountElement.textContent = String(gen++);
-    grid = next(grid);
-    updateGrid();
+    if (canvas.width !== width || canvas.height !== height) {
+      canvas.width = width;
+      canvas.height = height;
+      return true;
+    }
+
+    return false;
   }
-}, 200);
 
-function rafInterval(callback, interval) {
-  let last;
-  function run(timestamp) {
-    if (last === undefined) last = timestamp;
-    const elapsed = timestamp - last;
-    if (elapsed - interval) {
-      last = timestamp;
-      callback();
+  function requestAnimationFrame(cb, getInterval) {
+    let last;
+    function run(timestamp) {
+      if (last === undefined) last = timestamp;
+      const elapsed = timestamp - last;
+      if (elapsed > getInterval()) {
+        last = timestamp;
+        cb();
+      }
+      window.requestAnimationFrame(run);
     }
     window.requestAnimationFrame(run);
   }
-  window.requestAnimationFrame(run);
-}
 
-startButtonElement.addEventListener('click', () => {
-  isPlaying = !isPlaying;
-  startButtonElement.textContent = isPlaying ? 'Pause' : 'Start';
-});
+  return { render, resizeCanvasToDisplaySize, requestAnimationFrame };
+})();
+
+const grids = (() => {
+  function create(w, h, valFunc = (i, j) => false) {
+    const grid = [];
+    for (let i = 0; i < h; i++) {
+      const r = [];
+      for (let j = 0; j < w; j++) {
+        r.push(valFunc(i, j));
+      }
+      grid.push(r);
+    }
+
+    return grid;
+  }
+
+  function random(w, h, fractionLive = 0.25) {
+    const b = 0.8;
+    return create(w, h, (i, j) => {
+      return (
+        // Cover only the middle b% of the grid
+        i / h > 1 - b &&
+        i / h < b &&
+        j / w > 1 - b &&
+        j / w < b &&
+        Math.random() < fractionLive
+      );
+    });
+  }
+
+  function getPopulation(grid) {
+    let c = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        if (grid[i][j]) {
+          c++;
+        }
+      }
+    }
+
+    return c;
+  }
+
+  return { random, create, getPopulation };
+})();
+
+const game = (() => {
+  const generationsCountElement = document.getElementById('generations-count');
+  const populationCountElement = document.getElementById('population-count');
+  const startButtonElement = document.getElementById('start');
+  const clearButtonElement = document.getElementById('clear');
+  const randomButtonElement = document.getElementById('random');
+  const speedControlElement = document.getElementById('speed');
+  const speedValueElement = document.getElementById('speed-value');
+  const canvasElement = document.querySelector('canvas');
+
+  let cellWidthInPixels;
+
+  let _grid,
+    _isPlaying = false,
+    _numGens = 0,
+    w,
+    h,
+    speed;
+
+  function updateGrid(gridFunc) {
+    _grid = gridFunc(_grid);
+    graphics.render(canvasElement, _grid, cellWidthInPixels);
+    populationCountElement.textContent = String(grids.getPopulation(_grid));
+  }
+
+  function init() {
+    cellWidthInPixels = getCellWidth();
+    initSpeed();
+
+    w = Math.ceil(window.innerWidth / cellWidthInPixels);
+    h = Math.ceil(window.innerHeight / cellWidthInPixels);
+
+    startButtonElement.addEventListener('click', onClickPlay);
+    clearButtonElement.addEventListener('click', onClickClear);
+    randomButtonElement.addEventListener('click', onClickRandom);
+    speedControlElement.addEventListener('change', onChangeSpeed);
+
+    document.addEventListener('keydown', onKeyDown);
+
+    // Event listeners for dragging
+    {
+      let isMouseBeingDragged = false;
+      let dragValue; // value the cells dragged over should be
+      let isPlayingBeforeDrag;
+
+      canvasElement.addEventListener('mousedown', (evt) => {
+        isMouseBeingDragged = true;
+        isPlayingBeforeDrag = _isPlaying;
+        setIsPlaying(false);
+
+        const { col, row } = getRowAndCol(evt);
+        dragValue = !_grid[row][col];
+        updateGrid((grid) => setCellValue(grid, row, col));
+      });
+
+      canvasElement.addEventListener('mousemove', (evt) => {
+        if (isMouseBeingDragged) {
+          const { col, row } = getRowAndCol(evt);
+          updateGrid((grid) => setCellValue(grid, row, col));
+        }
+      });
+
+      canvasElement.addEventListener('mouseup', (evt) => {
+        if (isMouseBeingDragged) {
+          const { col, row } = getRowAndCol(evt);
+          updateGrid((grid) => setCellValue(grid, row, col));
+
+          isMouseBeingDragged = false;
+          isPlayingBeforeDrag && setIsPlaying(true);
+        }
+      });
+
+      function getRowAndCol(evt) {
+        return {
+          col: Math.floor(evt.clientX / cellWidthInPixels),
+          row: Math.floor(evt.clientY / cellWidthInPixels),
+        };
+      }
+
+      function setCellValue(grid, row, col) {
+        if (grid[row][col] === dragValue) {
+          return grid;
+        }
+
+        const newGrid = grid.map((row) => [...row]);
+        newGrid[row][col] = dragValue;
+        return newGrid;
+      }
+    }
+
+    graphics.resizeCanvasToDisplaySize(canvasElement);
+
+    // Start game
+    updateGrid(() => grids.create(w, h));
+    graphics.requestAnimationFrame(
+      () => {
+        if (_isPlaying) {
+          incrNumGens();
+          updateGrid((grid) => next(grid));
+        }
+      },
+      () => speed * 1000,
+    );
+  }
+
+  function getCellWidth() {
+    const url = new URL(window.location.href);
+    const size = url.searchParams.get('size');
+    switch (size) {
+      case 'lg':
+        return Math.max(
+          50,
+          Math.max(window.innerHeight / 50, window.innerWidth / 50),
+        );
+      default:
+        return 20;
+    }
+  }
+
+  function initSpeed() {
+    const url = new URL(window.location.href);
+    const speed = +url.searchParams.get('speed') || 1;
+    updateSpeed(speed);
+  }
+
+  function onChangeSpeed(evt) {
+    const gensPerSec = Math.pow(evt.target.value, 2); // from 0.1->7.07 to 0.01->50
+    updateSpeed(gensPerSec);
+  }
+
+  function updateSpeed(gensPerSec) {
+    speed = 1 / gensPerSec;
+    speedValueElement.textContent = gensPerSec.toFixed(2);
+    speedControlElement.value = Math.pow(gensPerSec, 0.5);
+  }
+
+  function onKeyDown(evt) {
+    if (!evt.ctrlKey && !evt.metaKey) {
+      switch (evt.key) {
+        case 'p':
+        case 'P':
+          onClickPlay();
+          break;
+        case 'c':
+        case 'C':
+          onClickClear();
+          break;
+        case 'r':
+        case 'R':
+          onClickRandom();
+          break;
+      }
+    }
+  }
+
+  function onClickPlay() {
+    toggleIsPlaying();
+  }
+
+  function onClickClear() {
+    setIsPlaying(false);
+    resetNumGens();
+    updateGrid(() => grids.create(w, h));
+  }
+
+  function onClickRandom() {
+    resetNumGens();
+    updateGrid(() => grids.random(w, h));
+  }
+
+  function incrNumGens() {
+    generationsCountElement.textContent = String(_numGens++);
+  }
+
+  function resetNumGens() {
+    _numGens = 0;
+    generationsCountElement.textContent = '0';
+  }
+
+  function toggleIsPlaying() {
+    setIsPlaying(!_isPlaying);
+  }
+
+  function setIsPlaying(isPlaying) {
+    _isPlaying = isPlaying;
+    startButtonElement.textContent = _isPlaying ? 'Pause (P)' : 'Play (P)';
+  }
+
+  return { init };
+})();
+
+game.init();
