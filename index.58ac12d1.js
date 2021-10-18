f1092179780c6d1bc6498ef6013ba7c99cac6f39
Chidi Williams <williamschidi1@gmail.com>
2021-10-18 19:29:26 +0100

    Updates

--- index.58ac12d1.js
+++ index.58ac12d1.js
@@ -705,7 +705,24 @@ const game = (()=>{
 })();
 game.init();
 
-},{"./demo-grids-helper":"dcTqh","./life":"bAcvN"}],"dcTqh":[function(require,module,exports) {
+},{"./life":"bAcvN","./demo-grids-helper":"dcTqh"}],"bAcvN":[function(require,module,exports) {
+function next(grid) {
+    const nextGrid = new Array(grid.length);
+    for(let i = 0; i < grid.length; i++)nextGrid[i] = new Array(grid[i].length);
+    for(let x = 0; x < grid.length; x++)for(let y = 0; y < grid[x].length; y++){
+        let c = 0;
+        for(let dx = -1; dx <= 1; dx++){
+            for(let dy = -1; dy <= 1; dy++)if (!(dx === 0 && dy === 0) && typeof grid[x + dx] !== 'undefined' && typeof grid[x + dx][y + dy] !== 'undefined' && grid[x + dx][y + dy]) c++;
+        }
+        nextGrid[x][y] = grid[x][y] ? c === 2 || c === 3 : c === 3;
+    }
+    return nextGrid;
+}
+module.exports = {
+    next
+};
+
+},{}],"dcTqh":[function(require,module,exports) {
 function create(w, h, valFunc = (i, j)=>false
 ) {
     const grid = [];
@@ -844,23 +861,6 @@ module.exports = {
     mirror,
     append,
     transpose
-};
-
-},{}],"bAcvN":[function(require,module,exports) {
-function next(grid) {
-    const nextGrid = new Array(grid.length);
-    for(let i = 0; i < grid.length; i++)nextGrid[i] = new Array(grid[i].length);
-    for(let x = 0; x < grid.length; x++)for(let y = 0; y < grid[x].length; y++){
-        let c = 0;
-        for(let dx = -1; dx <= 1; dx++){
-            for(let dy = -1; dy <= 1; dy++)if (!(dx === 0 && dy === 0) && typeof grid[x + dx] !== 'undefined' && typeof grid[x + dx][y + dy] !== 'undefined' && grid[x + dx][y + dy]) c++;
-        }
-        nextGrid[x][y] = grid[x][y] ? c === 2 || c === 3 : c === 3;
-    }
-    return nextGrid;
-}
-module.exports = {
-    next
 };
 
 },{}]},["fygJa","exVYn"], "exVYn", "parcelRequire4bfa")
